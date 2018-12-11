7aeece022c2981453782075f81b3eefb55ace327
NachisuK <eronek345@gmail.com>
2018-12-11 12:27:16 +0100

    Szablon Projektu

---
+++ StartGame.js
@@ -0,0 +1,12 @@
+const port = 3000; //Specify a port for our web server
+const express = require('express'); //load express with the use of requireJs
+var app =express();
+var path = require('path');
+
+app.get('/', function(req,res)
+{
+    res.sendFile(path.join(__dirname+'/MainView.html'));
+});
+app.listen(port, function() { //Listener for specified port
+    console.log("Server running at: http://localhost:" + port)
+});
