777e7bf1d97761124f993c007303be66094b2a2a
NachisuK <eronek345@gmail.com>
2018-12-11 13:20:27 +0100

    Zmiany organizacji plików

--- StartGame.js
+++ StartGame.js
@@ -3,10 +3,13 @@ const express = require('express'); //load express with the use of requireJs
 var app =express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname+'/')));
+
 app.get('/', function(req,res)
 {
     res.sendFile(path.join(__dirname+'/MainView.html'));
 });
+
 app.listen(port, function() { //Listener for specified port
     console.log("Server running at: http://localhost:" + port)
 });
