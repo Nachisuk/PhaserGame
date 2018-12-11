4f7e22ea51a96fd969a310547b611e2e5c6350cc
NachisuK <eronek345@gmail.com>
2018-12-11 23:02:49 +0100

    StrzelaniePodstawa

--- Gra/skrypty/Game.js
+++ Gra/skrypty/Game.js
@@ -22,8 +22,13 @@ var game = new Phaser.Game(config);
 //zmienne gry
 var background;
 var starsfield;
+var fireButton;
 var player;
+var speed;
+var bullets;
 var cursors;
+var firerate=0;
+var clock;
 
 function preload ()
 {
@@ -31,6 +36,7 @@ function preload ()
     this.load.image('background', 'assety/NebulaBlue.png');
     this.load.image('stars', 'assety/Stars.png');
     this.load.image('shipbasic','assety/ship-basic.png');
+    this.load.image('bullet','assety/bullet288.png');
 
 }
 
@@ -39,6 +45,7 @@ function create ()
     //środkowanie?
     //this.cameras.main.setBounds(-1024, -1024, 1024 * 2, 1024 * 2);
 
+    clock = Phaser.Time.Clock;
     //tworzenie tła
     background = this.add.image(1400, 800, 'background');
     starsfield =  this.add.tileSprite(0, 0, 2400, 1800, 'stars');
@@ -52,12 +59,17 @@ function create ()
     player.setCollideWorldBounds(true);
     player.onWorldBounds = true;
 
-
+    //tworzenie pocisków gracza
+    bullets = this.physics.add.group({
+        defaultKey: 'bullet',
+        maxSize: 40
+    })
     //sterowanie
     cursors = this.input.keyboard.createCursorKeys();
+    fireButton = this.input.keyboard.addKeys({ 'up': Phaser.Input.Keyboard.KeyCodes.W, 'down': Phaser.Input.Keyboard.KeyCodes.S });
 }
 
-function update ()
+function update (time,delta)
 {
     //'animacja' tła
     starsfield.tilePositionY -= 0.5;
@@ -83,4 +95,35 @@ function update ()
         {
             player.body.setVelocityY(200);
         }
+        if (fireButton.up.isDown)
+        {
+                fireBullet();
+        }
+
+    bullets.children.each(function (b){
+        if (b.active) {
+            if (b.body.y < -100) {
+                b.destroy();
+                b.setActive(false);
+            }
+        }
+    }.bind(this));
+}
+
+function fireBullet () {
+
+        if(Date.now() > firerate)
+        {
+            bullet = bullets.get(player.body.x+50,player.body.y)
+
+            if (bullet)
+            {
+                bullet.setActive(true);
+                bullet.setVisible(true);
+                bullet.body.setVelocityY(-300);
+                firerate = Date.now() +300;
+            }
+        }
+    
+
 }
